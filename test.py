06fd6efabe86c996d478b525d3844b3cc23c49eb
codenikko <codenikko@users.noreply.github.com>
2025-12-16 06:54:46 +0530

    just added this

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-git
+dict= {
+    "key": "value",
+    "matter": "a thing"
+}
+  
+
+  
+for i in dict:
+    print(i)           #prints all the keys
+    print(dict[i])   #prints all the keys with values below
+
+print(dict['key'])            #prints “value”
+dict["matter"]                #adds this to the dict on index 2
+dict["hesoyam"]= "cheat code" #adds key and value
