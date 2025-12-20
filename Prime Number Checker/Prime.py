1e727f5dc3ff19324f99a79f95e0cc62080b4ea7
codenikko <codenikko@users.noreply.github.com>
2025-12-20 22:51:16 +0530

    prime checker added

--- Prime Number Checker/Prime.py
+++ Prime Number Checker/Prime.py
@@ -3,10 +3,11 @@ def is_prime(num):
     for i in range(1, num+1):
         if num%i==0:
             c+=1
-
+    
     if c==2:
         print(f"{num} is a prime number")
     else:
         print(f"{num} is not a prime number") 
 
 is_prime(int(input("Enter your number of choice: \n")))
+
